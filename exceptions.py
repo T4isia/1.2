9ea03bf591993be37420806006b6f26079975572
Taisia <taechka.lu@rambler.ru>
2025-11-28 00:48:47 +0300

    Create custom exceptions for bookstore module
    Added custom exceptions for bookstore operations.

--- exceptions.py
+++ exceptions.py
@@ -1,1 +1,30 @@
+# Модуль с пользовательскими исключениями для книжного магазина
+
+class BookstoreError(Exception):
+    """Базовое исключение для книжного магазина"""
+    pass
+
+class BookNotFoundError(BookstoreError):
+    """Книга не найдена"""
+    pass
+
+class InsufficientQuantityError(BookstoreError):
+    """Недостаточное количество книг"""
+    pass
+
+class InvalidPriceError(BookstoreError):
+    """Неверная цена"""
+    pass
+
+class EmployeeNotFoundError(BookstoreError):
+    """Сотрудник не найден"""
+    pass
+
+class CustomerNotFoundError(BookstoreError):
+    """Клиент не найден"""
+    pass
+
+class FileOperationError(BookstoreError):
+    """Ошибка операции с файлом"""
+    pass
 
